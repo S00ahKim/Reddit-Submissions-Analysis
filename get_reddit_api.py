fdfc58d4d4aad5bfff1e9556b70f353fb346bb67
S00ahKim <rlatndk0206@naver.com>
2019-12-01 02:26:42 +0900

    API call을 함수 형식으로 변경

--- get_reddit_api.py
+++ get_reddit_api.py
@@ -25,137 +25,10 @@ def encode_roman(x):
     '''
     return str(x).encode('ascii', 'ignore').decode('ascii')
 
-def get_kpop(now, start_time, end_time):
+def cal_end_by_start(start):
     '''
-    r/kpop 처리
+    시작 시간을 인자로 주면 한달 뒤의 시간을 문자열로 리턴한다.
     '''
-    kpop = []
-    kpop_base_url = 'https://api.pushshift.io/reddit/submission/search/?subreddit=kpop&after={start}&before={end}&sort=asc'
-    kpop_done = False
-
-    while not kpop_done:
-        kpop_url = kpop_base_url.format(start = start_time, end = end_time)
-        kpop_r = requests.get(kpop_url).json()
-
-        if int(end_time) > int(now):
-            kpop_done = True
-
-        if len(kpop_r['data']) == 0:
-            if int(end_time) >= int(now):
-                kpop_done = True
-            else:
-                start_time = cal_time(end_time, 1)
-                end_time = cal_time(start_time, 10)
-        else:
-            end_created = 0
-            for kr in kpop_r['data']:
-                # 날짜 분리
-                timestamp = int(kr['created_utc'])
-                d = datetime.fromtimestamp(timestamp)
-                date = str(d.year)+'-'+str(d.month)+'-'+str(d.day)
-
-                # 문자열에 대해 인코딩 처리
-                uid = encode_roman(kr['id'])
-                title = encode_roman(kr['title'])
-                author = encode_roman(kr['author'])
-                try:
-                    if kr['selftext']:
-                        selftext = encode_roman(kr['selftext'])
-                    else:
-                        selftext = ''
-                except:
-                    print(kr)
-
-                # 데이터 저장
-                if len(kpop)>1 and d.month != kpop[-1][2]:
-                    kpop_done = True
-                else:
-                    tmp = [date, d.year, d.month, d.day, d.hour, uid, title, author, kr['created_utc'], selftext, kr['num_comments'], kr['score']]
-                    kpop.append(tmp)
-
-                # 마지막 날짜 기록용
-                end_created = kr['created_utc']
-
-            start_time = end_created
-            end_created = cal_time(start_time, 10)
-
-    kpop_save = pd.DataFrame(kpop)
-    kpop_save.columns = ['date', 'year', 'month', 'hour', 'day', 'id', 'title', 'author', 'created', 'selftext', 'num_comments', 'score']
-    dtm = datetime.fromtimestamp(int(start_time))
-    yy = str(dtm.year)
-    mm = str(dtm.month)
-    if len(mm) < 2:
-        mm = '0'+mm
-    save_file_name = './kpop/{yy}-{mm}.csv'.format(yy=yy, mm=mm)
-    kpop_save.to_csv(save_file_name, encoding='cp949')
-
-
-
-def get_kpoppers(now, start_time, end_time):
-    '''
-    r/kpoppers 처리
-    '''
-    kpoppers = []
-    kpoppers_base_url = 'https://api.pushshift.io/reddit/submission/search/?subreddit=kpoppers&after={start}&before={end}&sort=asc'
-    kpoppers_done = False
-
-    while not kpoppers_done:
-        kpoppers_url = kpoppers_base_url.format(start= start_time, end= end_time)
-        kpoppers_r = requests.get(kpoppers_url).json()
-
-        if int(end_time) > int(now):
-            kpoppers_done = True
-
-        if len(kpoppers_r['data']) == 0:
-            if int(end_time) >= int(now):
-                kpoppers_done = True
-            else:
-                start_time = cal_time(end_time, 1)
-                end_time = cal_time(start_time, 10)
-        else:
-            end_created = 0
-            for kp in kpoppers_r['data']:
-                # 날짜 분리
-                timestamp = int(kp['created_utc'])
-                d = datetime.fromtimestamp(timestamp)
-                date = str(d.year)+'-'+str(d.month)+'-'+str(d.day)
-
-                # 문자열에 대해 인코딩 처리
-                uid = encode_roman(kp['id'])
-                title = encode_roman(kp['title'])
-                author = encode_roman(kp['author'])
-                try:
-                    if kp['selftext']:
-                        selftext = encode_roman(kp['selftext'])
-                    else:
-                        selftext = ''
-                except:
-                    print(kp)
-
-                # 데이터 저장
-                if len(kpoppers) > 1 and d.month != kpoppers[-1][2]:
-                    kpoppers_done = True
-                else:
-                    tmp = [date, d.year, d.month, d.day, d.hour, uid, title, author, kp['created_utc'], selftext, kp['num_comments'], kp['score']]
-                    kpoppers.append(tmp)
-
-                # 마지막 날짜 기록용
-                end_created = kp['created_utc']
-
-            start_time = end_created
-            end_created = cal_time(start_time, 10)
-            
-    kpoppers_save = pd.DataFrame(kpoppers)
-    kpoppers_save.columns = ['date', 'year', 'month', 'day', 'hour', 'id', 'title', 'author', 'created', 'selftext', 'num_comments', 'score']
-    dtm = datetime.fromtimestamp(int(start_time))
-    yy = str(dtm.year)
-    mm = str(dtm.month)
-    if len(mm) < 2:
-        mm = '0'+mm
-    save_file_name = './kpoppers/{yy}-{mm}.csv'.format(yy=yy, mm=mm)
-    kpoppers_save.to_csv(save_file_name, encoding='cp949')
-
-def cal_now_by_start(start):
     tmp = int(start)
     dttime = datetime.fromtimestamp(tmp)
     wttime = dttime + relativedelta(months=1)
@@ -178,12 +51,77 @@ def get_start_times(wanted_start, wanted_end):
             arr.append(int(timestamp))
     return arr
 
-#start_times = get_start_times('2013-06', '2017-12')
-start_times = get_start_times('2017-01', '2017-11')
+def get_reddit(start_time, end_time, subreddit):
+    '''
+    r/subreddit의 submissions를 start_time부터 end_time까지 긁는다.
+    argument 타입은 모두 string
+    '''
+    arr = []
+    base_url = 'https://api.pushshift.io/reddit/submission/search/?subreddit={subreddit}&after={start}&before={end}&sort=asc'
+    get_all = False
+
+    dtm = datetime.fromtimestamp(int(start_time))
+    yy = str(dtm.year)
+    mm = str(dtm.month)
+
+    while not get_all:
+        next_time = cal_time(start_time, 10)
+        url = base_url.format(subreddit=subreddit, start=start_time, end=next_time)
+        r = requests.get(url).json()
+        last_get_time = 0
+
+        if len(r['data']) == 0:
+            if int(last_get_time) >= int(end_time):
+                get_all = True
+            else:
+                start_time = next_time
+                next_time = cal_time(start_time, 10)
+        
+        else:
+            for submission in r['data']:
+                # 날짜 분리
+                timestamp = int(submission['created_utc'])
+                d = datetime.fromtimestamp(timestamp)
+                date = str(d.year)+'-'+str(d.month)+'-'+str(d.day)
+
+                # 문자열에 대해 인코딩 처리
+                uid = encode_roman(submission['id'])
+                title = encode_roman(submission['title'])
+                author = encode_roman(submission['author'])
+                try:
+                    if submission['selftext']:
+                        selftext = encode_roman(submission['selftext'])
+                    else:
+                        selftext = ''
+                except:
+                    print(submission)
+
+                # 데이터 저장
+                if len(arr)>1 and d.month != arr[-1][2]:
+                    get_all = True
+                else:
+                    tmp = [date, d.year, d.month, d.day, d.hour, uid, title, author, submission['created_utc'], selftext, submission['num_comments'], submission['score']]
+                    arr.append(tmp)
+
+                # 마지막 날짜 기록용
+                last_get_time = submission['created_utc']
+
+            start_time = last_get_time
+            next_time = cal_time(start_time, 10)
+
+    kpop_save = pd.DataFrame(arr)
+    kpop_save.columns = ['date', 'year', 'month', 'hour', 'day', 'id', 'title', 'author', 'created', 'selftext', 'num_comments', 'score']
+    
+    if len(mm) < 2:
+        mm = '0'+mm
+
+    path = './{dirname}/'.format(dirname = subreddit)
+    save_file_name = path + '{yy}-{mm}.csv'.format(yy=yy, mm=mm)
+    kpop_save.to_csv(save_file_name, encoding='cp949')
+
+
+start_times = get_start_times('2013-06', '2017-12')
 for start_time in start_times:
     print(start_time)
-    end_time = cal_time(start_time, 10)
-    now = cal_now_by_start(start_time)
-
-    get_kpop(now, start_time, end_time)
-    get_kpoppers(now, start_time, end_time)
+    end_time = cal_end_by_start(start_time)
+    get_reddit(start_time, end_time, 'kpop')
